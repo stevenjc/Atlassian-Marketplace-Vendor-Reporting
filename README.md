6878f8a38f5b468c4722e668b54a11156396bf30
Steven Colon <stevenjc@users.noreply.github.com>
2020-06-02 15:42:08 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# Atlassian-Marketplace-Vendor-Reporting
+# Atlassian Marketplace Vendor Search
+
+Purpose: Correlate a list of emails and emails domains to transactions/licenses in the Atlassian Marketplace automatically.
+
+This python utility takes lists of search terms as single column CSV files and searchs for active licenses and transactions (within 30days) tied to those terms. The output is a licenses and transactions csv files for each 
+
+## Important Note 
+
+Created by Steven Colon.
+
+This script is not supported by SmartBear Software
+
+## Install Instructions 
+
+Made on Python 3.7.1
+
+Requires the requests python library - http://docs.python-requests.org/en/master/ 
+
+Download Repo locally
+
+## Configuration Instructions
+config.json file is required in the same directory as the migration script. 
+
+Example config can be found 
+
+REPORT_DIR: Output Directory for Reports.
+
+VENDOR_ID: Vendor ID issued by Atlassian.
+
+ADD_ON: ID of Vendor Add-on
+
+AUTH_EMAIL: Email of account with Reporting Permissions to Vendor Account.
+
+AUTH_PW: Password of account with Reporting Permissions to Vendor Account.
+
+SEARCH_FILES: Array(List) of files that contain search text *NOTE Files should be single column csv with search terms.
+
+All fields are required. 
+
+
+## Execution Instructions
+
+Execute script 
+`python email_lookup.py`
+
+## License
+
+   Copyright 2020 Steven Colon
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+
+
---
+++ email_lookup.py
@@ -0,0 +1,118 @@
+import json
+import os
+import requests
+from datetime import datetime, timedelta
+import boto3, botocore
+import csv
+
+with open('config.json', 'r') as file:
+    config = json.load(file)
+
+
+auth_email=config['AUTH_EMAIL']
+auth_pw=config['AUTH_PW']
+report_dir=config['REPORT_DIR']
+search_files=config["SEARCH_FILES"]
+vendor_id=config["VENDOR_ID"]
+addon=config["ADD_ON"]
+
+base_url = "https://marketplace.atlassian.com/rest/2/vendors/"+vendor_id+"/reporting/"
+
+def license_lookup(search_text, time_range, index, row_num, inputfile):
+    print("Searching for - " + search_text)  
+    #Starting date for the query. Format: Date
+    start_date= str(datetime.strftime(datetime.now() - timedelta(int(time_range)), '%Y-%m-%d'))
+    #End date for the query. Format: Date
+    end_date=str(datetime.strftime(datetime.now(), '%Y-%m-%d'))
+    #The date field against which filters will be applied. Valid values: end, start
+    date_type="start"
+    #Text to search for in license fields (license id, customer information and partner information)
+    text=search_text
+    #If specified, retrieve marketing funnel attribution data
+    with_attribution="false"
+    #Specifies the response format. If unspecified, the 'Accept' header will be used.Valid values: csv, json
+    accept="csv"
+    export_licenses= base_url+"licenses/export?addon="+addon+"&text="+text+"&withAttribution="+with_attribution+"&accept="+accept+"&status=active"
+    
+    #"&startDate="+start_date+"&endDate="+end_date+"&dateType="+date_type+
+    print(export_licenses)
+    
+    info = requests.get(export_licenses, auth=(auth_email, auth_pw))
+    # Write to .CSV
+    filename = report_dir+'licenses_'+inputfile
+    f = open(filename, "a")
+    split = info.text.split("\n")
+    print(str(index))
+    line_num=0 #track line number in single API call
+    for line in split:
+        if index is not 0:
+            if line_num is not 0:
+                print(str(line))
+                f.write(line + "\n")
+        elif index is 0:
+            if row_num is 0:
+                print(str(line))
+                f.write(line + "\n")
+            elif line_num is not 0:
+                print(str(line))
+                f.write(line + "\n")
+        line_num = line_num + 1
+    f.close()
+
+def transactions_lookup(search_text, time_range, index, row_num, inputfile):
+    print("Searching for - " + search_text)  
+    #Starting date for the query. Format: Date
+    start_date= str(datetime.strftime(datetime.now() - timedelta(int(time_range)), '%Y-%m-%d'))
+    #End date for the query. Format: Date
+    end_date=str(datetime.strftime(datetime.now(), '%Y-%m-%d'))
+    #The date field against which filters will be applied. Valid values: end, start
+    date_type="start"
+    #Text to search for in license fields (license id, customer information and partner information)
+    text=search_text
+    #Specifies the response format. If unspecified, the 'Accept' header will be used.Valid values: csv, json
+    accept="csv"
+    export_transactions=base_url+"sales/transactions/export?addon="+addon+"&text="+text+"&accept="+accept+"&startDate="+start_date+"&endDate="+end_date+"&dateType="+date_type
+    
+    #"&startDate="+start_date+"&endDate="+end_date+"&dateType="+date_type+
+    print(export_transactions)
+    
+    info = requests.get(export_transactions, auth=(auth_email, auth_pw))
+    # Write to .CSV
+    filename = report_dir+'transactions_'+inputfile
+    f = open(filename, "a")
+    split = info.text.split("\n")
+    print(str(index))
+    line_num=0 # track line number in single API call
+    for line in split:
+        if index is not 0:
+            if line_num is not 0:
+                print(str(line))
+                f.write(line + "\n")
+        elif index is 0:
+            if row_num is 0:
+                print(str(line))
+                f.write(line + "\n")
+            elif line_num is not 0:
+                print(str(line))
+                f.write(line + "\n")
+        line_num = line_num + 1
+    f.close()
+    
+if __name__ == '__main__':
+    index = 0 # track file number
+    for filename in search_files:
+        with open(filename) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            row_num = 0 # track row number in a single file
+            for row in readCSV:
+                license_lookup(row[0],'60', index, row_num, filename)
+                row_num = row_num + 1
+        index = index + 1
+    index = 0
+    for filename in search_files:
+        with open(filename) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            row_num = 0
+            for row in readCSV:
+                transactions_lookup(row[0],'30', 0, row_num, filename)
+                row_num = row_num + 1
